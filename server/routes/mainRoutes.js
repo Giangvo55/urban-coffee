abf2bcc9c8ad468ec62c4bebbaa40d7b89f7096e
Giangvo55 <giang.vochi5@gmail.com>
2022-01-01 13:30:36 +0700

    load product in Homepage

--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -7,4 +7,5 @@ const mainController = require('../controllers/mainController');
  * App routes
  */
 router.get('/', mainController.homepage);
+router.get('/drinks/:id', mainController.exploreDrinksDetail)
 module.exports = router;
