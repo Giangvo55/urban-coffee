5446293fc4a337e3b1975dd7100b9b5912285d85
Giangvo55 <giang.vochi5@gmail.com>
2022-01-03 00:41:51 +0700

    new function add to cart

--- server/models/Drink.js
+++ server/models/Drink.js
@@ -1,34 +1,35 @@
-const { Int32 } = require('mongodb');
 const mongoose = require('mongoose');
 
 
 const drinkSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: 'This field is required.'
-    },
-    price: {
-      type: Number,
-      required: 'This field is required.'
-    },
-    description: {
-        type: String,
-        required: 'This field is required.'
-      },
-    size: {
-        type: String,
-        required: 'This field is required.'
-      },
-    img: {
-        type: Array,
-        required: 'This field is required.'
-      },
-    category: {
-        type: String,
-        enum: ['Cà phê', 'Trà trái cây', 'Cà phê bột', 'Bánh-Snacks'],
-        required: 'This field is required.'
-      },
+  name: {
+    type: String,
+    required: 'This field is required.'
+  },
+  price: {
+    type: Number,
+    required: 'This field is required.'
+  },
+  description: {
+    type: String
+  },
+  size: {
+    type: String,
+    required: 'This field is required.'
+  },
+  img: {
+    type: Array,
+    required: 'This field is required.'
+  },
+  category: {
+    type: String,
+    enum: ['Cà phê', 'Trà trái cây', 'Cà phê bột', 'Bánh-Snacks'],
+    required: 'This field is required.'
+  },
+  slug: {
+    type: String
+  }
 
-  });
-  
+});
+
 module.exports = mongoose.model('Drink', drinkSchema);
